3e0eded94b8b696d8922a7ae9b1f0d48cc96d2d7
gabxetc <gvchitamu@gmail.com>
2024-10-27 22:40:52 +0200

    Cleaning out commets. Final Push

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const PORT = 8000
 
 // Getting the package and storing it in these variable names
 
-// import axios from 'axios';  // library that allows you to make HTTP requests
-// import { load } from 'cheerio'; // allows to extract info from html documents
-// import express, { json } from 'express'; // Handles the server
-// import { writeFile } from 'fs'; // built in file system that handles reading, writing and updating files
-// import cors from 'cors'; // cross origin resource sharing
-
 const axios = require('axios');  // library that allws you to make HTTP requests
 const cheerio = require('cheerio'); // allows to extract info from html documents
 const express = require('express'); // Handles the server
 const fs = require('fs'); // built in file system that handles reading, writing and updating files
 const cors = require('cors'); // cross origin resource sharing
 const scrapeProperties = require('./scrape');
-const scrapePropertiesPromotedImage = require('./scrape');
 
 let server;
 
@@ -33,11 +26,8 @@ app.post('/scrape', (req, res) => { // Extracting the url from the /scrape endpo
 
 console.log(url);
 
-
 axios(url) // Chaining: Returns a promise THEN we get the reponse of whatevers come back.
     .then(response => {
-        // console.log("Response Status:", response.status); // Check the response status
-        // console.log("Response Data:", response.data);
         const html = response.data // We get the response data and save it as HTML
         const $ = cheerio.load(html)
         const properties = [];
@@ -66,7 +56,3 @@ fs.writeFile('html/properties.json', JSON.stringify(properties, null, 2), (err) 
 server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
-
-// const montgomery park = https://www.property24.com/to-rent/montgomery-park/johannesburg/gauteng/5780
-// const rivonia = https://www.property24.com/to-rent/rivonia/sandton/gauteng/4251
